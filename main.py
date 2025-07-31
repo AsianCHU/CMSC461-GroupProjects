4b279549a13ecce0a26c8e8fca901345b89c08b9
Abdullah Nouman <anouman@bloombergindustry.com>
2025-07-31 14:42:16 -0400

    added more small changes to  python/flask setup

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import sqlite3
-from flask import Flask, render_template, g
+from flask import Flask, render_template, g, request
 
 db_name = ''
 
 # Connect to database
-def get_database(db_name):
+def get_db(db_name):
     if 'db' not in g:
         g.db = sqlite3.connect(db_name)
 
@@ -19,10 +19,14 @@ def close_db(e=None):
 # Sets up server to run app
 app = Flask(__name__)
 
+# Route that will handle get, post, and delete operations on db
 @app.route('/')
-@app.route('/home')
+@app.route('/home', methods=["GET", "POST", "DELETE"])
 def index():
-    return render_template('index.html')
+    if request.method == "GET":
+        return render_template('index.html') 
+
+    return render_template('index.html') # Returns content on html page
 
 if __name__ == '__main__':
     app.run(debug=False)
