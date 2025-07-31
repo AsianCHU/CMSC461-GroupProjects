04801ba32b906e8eeb6fca23d9cc950193ed69b6
Abdullah Nouman <anouman@bloombergindustry.com>
2025-07-31 15:07:35 -0400

    Added sqlite3 setup & functions for in python file

--- README.md
+++ README.md
@@ -10,25 +10,3 @@ Make sure to check the github for any changes and pull those changes onto local 
 desktop and press pull
 
 Make sure you always push any changes and make notes on what you did (github desktop wont even let you push without a description anyways)
-
-Running python application:
-# Step 1: Setup virtual environment
-python3 -m venv venv
-
-# Step 2: Activate it
-source venv/bin/activate (Mac)
-venv\Scripts\activate (Windows)
-
-# Step 3: Install dependencies
-pip install -r packages.txt
-
-# Step 4: Run app
-In project directory run:
-export FLASK_APP=main.py (Mac)
-set FLASK_APP=main.py (Windows)
-flask run
-
-/templates is where HTML will be stored and UI for the application
-main.py is application where we will add our logic to our database
-We will need a .db file to be able to connect with Python
-packages.txt is all of the packages needed to install to run app
--- main.py
+++ main.py
@@ -1,32 +1,20 @@
 import sqlite3
-from flask import Flask, render_template, g, request
 
 db_name = ''
+connection = sqlite3.connect(db_name)
+cursor = connection.cursor()
 
-# Connect to database
-def get_db(db_name):
-    if 'db' not in g:
-        g.db = sqlite3.connect(db_name)
+def insertOffice(name, city, area):
+    cursor.execute('INSERT INTO office (name, city, area) VALUES (?, ?, ?)', 
+    (name, city, area))
+    connection.commit
 
-    return g.db
+def getOffice():
+    cursor.execute('SELECT * FROM office')
 
-def close_db(e=None):
-    db = g.pop('db', None)
+def deleteOffice(name):
+    cursor.execute('DELETE FROM office WHERE name = ?', name)
 
-    if db is not None:
-        db.close()
 
-# Sets up server to run app
-app = Flask(__name__)
-
-# Route that will handle get, post, and delete operations on db
-@app.route('/')
-@app.route('/home', methods=["GET", "POST", "DELETE"])
-def index():
-    if request.method == "GET":
-        return render_template('index.html') 
-
-    return render_template('index.html') # Returns content on html page
-
-if __name__ == '__main__':
-    app.run(debug=False)
+#at end of program
+connection.close()
